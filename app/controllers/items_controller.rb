12a3ab3fb48154ba0dc741e19e2b59c21119f842
NaoFarron <bk201-nao@live.jp>
2021-09-25 03:37:19 +0900

    ビューファイル配置

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,16 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  def new
+  end
+
+  def create
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:title, :explanation, :category_id, :item_Status_id, :delivery_fee_id, :prefecture_id, :estimated_shipping_date_id, :price,  :image).merge(user_id: current_user.id)
+  end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,14 @@
 class Item < ApplicationRecord
   belongs_to :user
+  has_one_attached :image
+
+  validates :title,                      presence: true
+  validates :explanation,                presence: true
+  validates :category_id,                presence: true
+  validates :item_Status_id,             presence: true
+  validates :delivery_fee_id,            presence: true
+  validates :prefecture_id,              presence: true
+  validates :estimated_shipping_date_id, presence: true
+  validates :price,                      presence: true
+  validates :image,                      presence: true
 end
