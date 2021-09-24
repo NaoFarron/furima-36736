9cf7eaae2b9a7614dba44463ad65f40a97914ab1
NaoFarron <bk201-nao@live.jp>
2021-09-24 23:19:30 +0900

    商品出品情報を保存するためのモデルとテーブルを作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,23 +1,25 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_many :items
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-         VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-         validates :password, format: {with: VALID_PASSWORD_REGEX}, allow_blank: true
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates :password, format: {with: VALID_PASSWORD_REGEX}, allow_blank: true
 
-         validates :nickname, presence: true
-         
-         with_options format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, allow_blank: true }, presence: true do
-          validates :last_name
-          validates :first_name
-         end
+  validates :nickname, presence: true
+  
+  with_options format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, allow_blank: true }, presence: true do
+  validates :last_name
+  validates :first_name
+  end
 
-         with_options format: { with: /\A[ァ-ヶ]+\z/, allow_blank: true }, presence: true do
-          validates :last_name_kana
-          validates :first_name_kana
-         end
-         
-         validates :birthday, presence: true
+  with_options format: { with: /\A[ァ-ヶ]+\z/, allow_blank: true }, presence: true do
+  validates :last_name_kana
+  validates :first_name_kana
+  end
+  
+  validates :birthday, presence: true
 end
