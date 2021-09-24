86f7882c1f715f3df6a4cb4d1e340972ea6170ef
NaoFarron <bk201-nao@live.jp>
2021-09-24 20:17:10 +0900

    コードレビュー修正3

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,18 +9,15 @@ class User < ApplicationRecord
 
          validates :nickname, presence: true
          
-         VALID_NAME_REGEX = /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ 
-          validates :last_name, format: {with: VALID_NAME_REGEX}, allow_blank: true
-          validates :last_name, presence: true
-          validates :first_name, format: {with: VALID_NAME_REGEX}, allow_blank: true
-          validates :first_name, presence: true
+         with_options format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, allow_blank: true }, presence: true do
+          validates :last_name
+          validates :first_name
+         end
 
-         VALID_NAME_KANA_REGEX = /\A[ァ-ヶ]+\z/
-          validates :last_name_kana, format: { with: VALID_NAME_KANA_REGEX }, allow_blank: true 
-          validates :last_name_kana, presence: true
-          validates :first_name_kana, format: { with: VALID_NAME_KANA_REGEX }, allow_blank: true 
-          validates :first_name_kana, presence: true
-         
+         with_options format: { with: /\A[ァ-ヶ]+\z/, allow_blank: true }, presence: true do
+          validates :last_name_kana
+          validates :first_name_kana
+         end
          
          validates :birthday, presence: true
 end
