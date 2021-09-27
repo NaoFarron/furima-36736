ca5b7bd85cd06d898591df8aa0c2b581409f383e
NaoFarron <bk201-nao@live.jp>
2021-09-27 17:23:26 +0900

    コードレビュー修正4

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -3,16 +3,12 @@ window.addEventListener('load', () => {
   const priceInput = document.getElementById("item-price");
   priceInput.addEventListener("input", () => {
     const inputValue = priceInput.value;
-    inputValue;
     
     const addTaxDom = document.getElementById("add-tax-price");
     addTaxDom.innerHTML = (Math.floor(inputValue * 0.1));
-    addTaxDom;
 
     const profitNumber = document.getElementById("profit")
     const value_result = inputValue * 0.1
-    value_result
     profitNumber.innerHTML = (Math.floor(inputValue - value_result));
-      profitNumber;
   })
 });
