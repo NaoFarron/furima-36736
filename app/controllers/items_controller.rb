8f1aae75023b3b2df54dee4f8f9336301f72cee5
NaoFarron <bk201-nao@live.jp>
2021-09-28 19:45:01 +0900

    商品情報編集機能作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new]
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_item, except: [:index, :new, :create]
+  before_action :seller_confirmation, only: [:edit, :update,]
 
   def index
     @items = Item.includes(:user).order("created_at DESC")
@@ -22,11 +24,31 @@ class ItemsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @item.update(item_params)
+      redirect_to item_path
+    else
+      render :edit
+    end
+
+  end
+
   private
 
   def item_params
     params.require(:item).permit(:title, :explanation, :category_id, :item_status_id, :delivery_fee_id, :prefecture_id,
                                  :estimated_shipping_date_id, :price, :image).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
+  def seller_confirmation
+    redirect_to root_path unless current_user == @item.user
   end
 
 end
